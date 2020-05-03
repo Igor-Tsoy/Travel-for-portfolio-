86a79576d4d26f36525e7e5d8d0a0b0a331223dc
Igor <aksel-zig@mail.ru>
2020-05-03 16:48:35 +0700

    --//--

--- travel/settings/product.py
+++ travel/settings/product.py
@@ -94,7 +94,8 @@ DATABASES = {
 
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db = dj_database_url.config()
+DATABASES['default'].update(db)
 
 
 # Password validation
@@ -133,6 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
-django_heroku.settings(locals())
+
+
